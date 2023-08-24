095460d4b1fe665427f1fe590fb4afbf242bcd5a
TH4YSZ <thainacristina.sz07@gmail.com>
2023-08-24 09:57:00 -0300

    Criação do menu

--- algoritmo.py
+++ algoritmo.py
@@ -1,3 +1,26 @@
+import os
 from classes import *
 
-menu = int(input("BEM-VINDO"))
+hotel = Hotel()
+
+a = 1
+while a == 1:
+    
+    try:
+        menu = int(input("BEM-VINDO MALTVAGO! \n[1] Login \n[2] Cadastro \n[3] Sair \nDigite a opção desejada: "))
+        if menu == 1:
+            os.system("cls")
+            ccpf = int(input("Digite o CPF: "))
+            csenha = input("Digite a senha: ")
+            confirmar_cpf = Hotel(ccpf)
+            confirmar_senha = Hotel(csenha)
+        
+     
+        if menu == 2:
+            print("teste")
+
+        if menu == 3:
+            a = 0
+            
+    except Exception as erro:
+        print("Opção inválida.")

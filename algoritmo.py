d616e0ea23d8254ec85c37402698e8b1d433e95f
TH4YSZ <thainacristina.sz07@gmail.com>
2023-08-24 10:32:07 -0300

    Cadastro e login

--- algoritmo.py
+++ algoritmo.py
@@ -12,15 +12,24 @@ while a == 1:
             os.system("cls")
             ccpf = int(input("Digite o CPF: "))
             csenha = input("Digite a senha: ")
-            confirmar_cpf = Hotel(ccpf)
-            confirmar_senha = Hotel(csenha)
-        
+            hotel.login(ccpf,csenha)
+            
+            
+
      
         if menu == 2:
-            print("teste")
+            os.system("cls")
+            print("Preencha as informações: \n")
+            nome = input("Digite seu nome: ")
+            telefone = int(input("Digite seu telefone: "))
+            email = input("Digite seu E-mail: ")
+            cpf = int(input("Digite seu cpf: "))
+            senha = input("Digite sua senha :")
+            hotel.cliente(nome, telefone, email, cpf, senha)
+            
 
         if menu == 3:
             a = 0
             
     except Exception as erro:
-        print("Opção inválida.")
+        print(erro.__class__.__name__)
